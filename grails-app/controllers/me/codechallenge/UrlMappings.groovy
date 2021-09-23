5ef19774816bd4bcb33a46c755a495af6469488b
maeis <maeis.janian@rxlogix.com>
2021-09-23 10:47:08 +0330

    improv

--- grails-app/controllers/me/codechallenge/UrlMappings.groovy
+++ grails-app/controllers/me/codechallenge/UrlMappings.groovy
@@ -2,10 +2,12 @@ package me.codechallenge
 
 class UrlMappings {
 
+    static String version = "v1"
+
     static mappings = {
-        get "/api/v1/$controller(.$format)?"(action:"index")
-        get "/api/v1/$controller/$id(.$format)?"(action:"show")
-        post "/api/v1/$controller(.$format)?"(action:"save")
+        get "/api/${version}/$controller(.$format)?"(action:"index")
+        get "/api/${version}/$controller/$id(.$format)?"(action:"show")
+        post "/api/${version}/$controller(.$format)?"(action:"save")
 
         "/"(controller: 'event', action:'index')
         "500"(view: '/error')
